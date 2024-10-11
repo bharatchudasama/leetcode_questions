238dff0e1db5ff8b3424c57d4dc528a772008f47
Bharat chudasama <90544217+bharatchudasama@users.noreply.github.com>
2024-10-11 20:26:31 +0530

    Time: 4 ms (74.76%), Space: 11.3 MB (80.14%) - LeetHub

--- 0120-triangle/0120-triangle.cpp
+++ 0120-triangle/0120-triangle.cpp
@@ -1,20 +1,22 @@
-//tabulation
+//space optimiztion
 class Solution {
 
 public:
     int minimumTotal(vector<vector<int>>& arr) {
         int n = arr.size();
-        vector<vector<int>> dp(n+1,vector<int>(n+1,-1)); 
+        vector<int> front(n);
+        vector<int> dp(n);
         for(int j=0 ; j<n ;j++){
-            dp[n-1][j] = arr[n-1][j];
+            front[j] = arr[n-1][j];
         }
         for(int i  = n-2 ; i>=0 ;i--){
             for(int j=i ; j>=0 ;j--){
-                int d=arr[i][j] + dp[i+1][j];
-                int dg=arr[i][j] + dp[i+1][j+1];
-                dp[i][j] = min(d,dg);
+                int d=arr[i][j] + front[j];
+                int dg=arr[i][j] + front[j+1];
+                dp[j] = min(d,dg);
             }
+            front = dp;
         }
-        return dp[0][0];
+        return front[0];
     }
 };
