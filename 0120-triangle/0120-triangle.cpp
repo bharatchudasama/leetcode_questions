ac637a5b4ec8e3fab5c29ec4fbec6310ff49c777
Bharat chudasama <90544217+bharatchudasama@users.noreply.github.com>
2024-10-11 20:24:07 +0530

    Time: 3 ms (84.96%), Space: 11.8 MB (35.24%) - LeetHub

--- 0120-triangle/0120-triangle.cpp
+++ 0120-triangle/0120-triangle.cpp
@@ -1,16 +1,20 @@
+//tabulation
 class Solution {
-private:
-    int f(int i ,int j , int n, vector<vector<int>>& dp , vector<vector<int>>& arr){
-        if(i==n-1){return arr[i][j];}
-        if(dp[i][j]!=-1){return dp[i][j];}
-        int d = arr[i][j] + f(i+1,j,n,dp,arr);
-        int dg = arr[i][j] + f(i+1,j+1,n,dp,arr);
-        return dp[i][j] = min(d,dg);
-    }
+
 public:
-    int minimumTotal(vector<vector<int>>& triangle) {
-        int n = triangle.size();
+    int minimumTotal(vector<vector<int>>& arr) {
+        int n = arr.size();
         vector<vector<int>> dp(n+1,vector<int>(n+1,-1)); 
-        return f(0,0,n,dp,triangle);
+        for(int j=0 ; j<n ;j++){
+            dp[n-1][j] = arr[n-1][j];
+        }
+        for(int i  = n-2 ; i>=0 ;i--){
+            for(int j=i ; j>=0 ;j--){
+                int d=arr[i][j] + dp[i+1][j];
+                int dg=arr[i][j] + dp[i+1][j+1];
+                dp[i][j] = min(d,dg);
+            }
+        }
+        return dp[0][0];
     }
 };
